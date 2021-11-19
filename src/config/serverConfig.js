6a51eb5274249a5426ea3be68e8fae7ef12cbdc0
marufdeen <marufajagunna@gmail.com>
2021-11-19 15:39:08 +0100

    modified

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,6 +1,6 @@
 const config = {
   appName: process.env.APP_NAME,
-  port: process.env.APP_PORT,
+  port: process.env.APP_PORT || 5000,
   url: process.env.APP_URL || 'http://127.0.0.1',
   enviroment: process.env.APP_ENV || 'production',
 };
